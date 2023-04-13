80b5f860692a3b2b496624ea8b1be7485c86d0c4
Userrrrnnnaaammmeee228 <rvova2003@bk.ru>
2023-04-13 12:55:02 +0300

    ver.0.2.1

--- main_Strelnikov.py
+++ main_Strelnikov.py
@@ -5,10 +5,8 @@ def generate_password(length=11):
     """Generate a random password of given length (default 11)"""
     # define possible characters
     chars = string.ascii_letters + string.digits + string.punctuation
-
     # generate password
     password = ''.join(random.choice(chars) for i in range(length))
-
     return password
 
 # example usage
